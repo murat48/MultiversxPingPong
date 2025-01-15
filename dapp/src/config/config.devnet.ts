cd1b0ab52697d709ec299dcd655ca708e752039b
Halfgork <gorkemsakalak@gmail.com>
2025-01-15 22:07:09 +0300

    Update contract address in devnet configuration

--- dapp/src/config/config.devnet.ts
+++ dapp/src/config/config.devnet.ts
@@ -3,7 +3,7 @@ import { EnvironmentsEnum } from 'types';
 export * from './sharedConfig';
 
 export const contractAddress =
-  'erd1qqqqqqqqqqqqqpgqm6ad6xrsjvxlcdcffqe8w58trpec09ug9l5qde96pq';
+  'erd1hh8qjzht9rgeux9gftwhx0djh4ay3ql2ssx83l6c3qjsnymzqtwqa7f4du';
 export const API_URL = 'https://devnet-template-api.multiversx.com';
 export const sampleAuthenticatedDomains = [API_URL];
 export const environment = EnvironmentsEnum.devnet;
